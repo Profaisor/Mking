0346d8a4e3f85f63d9919544ba238fb59ccef6f5
Profaisor <168448303+Profaisor@users.noreply.github.com>
2024-04-29 19:14:23 +0330

    Update run.py

--- run.py
+++ run.py
@@ -4,12 +4,6 @@ os.system('git pull')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('Mking64.so'):
-        os.system('curl -L https://github.com/Profaisor/files/blob/main/Mking64.cpython-311.so?raw=true -o Mking64.so') 
-        import mking_menu
-    else:
-        import mking_menu
-
+	import Mking64
 elif bit == '32bit':
-    if not os.path.isfile('Mking32.so'):
-        exit('Your Device 32 Bit Not Work For You') 
+    exit('Your Device 32 Bit Not Work For You') 
